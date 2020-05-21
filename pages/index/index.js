d1fc3b56cdbcd3df2642aa8799de64766619e74d
tianzerun <tian.ze@husky.neu.edu>
2020-05-21 18:00:48 +0800

    use dynamic data from weather api

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,32 @@
-//index.js
-//获取应用实例
-const app = getApp()
+const weatherMap = {
+  'sunny': 'sunny',
+  'cloudy': 'cloudy',
+  'overcast': 'overcast',
+  'lightrain': 'light rain',
+  'heavyrain': 'heavy rain',
+  'snow': 'snow'
+ }
 
-Page({})
+Page({
+  data: {
+    nowTemp: '',
+    nowWeather: ''
+  },
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: "Guangzhou"
+      },
+      success: res => {
+        let result = res.data.result;
+        let temp = result.now.temp;
+        let weather = result.now.weather;
+        this.setData({
+          nowTemp: `${temp}°`,
+          nowWeather: weatherMap[weather]
+        });
+      }
+    })
+  }
+})
